0841702e60801e7fc51b2e612748d2619b7672f3
kev1129 <shinado1129@gmail.com>
2024-06-03 20:06:15 +0900

    change google analytics id

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,7 @@ export default {
       { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.8.1/css/all.css', integrity: 'sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf', crossorigin: 'anonymous' },
     ],
     googleAnalytics: {
-      id: '139614959',
+      id: 'G-0R60E7END0',
       debug: false
     }
   },
